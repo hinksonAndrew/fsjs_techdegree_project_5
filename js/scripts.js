2e404261a16da6153d363bbdf11af20e636388c3
Andrew Hinkson <38588648+hinksonAndrew@users.noreply.github.com>
2020-10-01 18:41:50 -0500

    basic function complete

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,7 @@
 const url = 'https://randomuser.me/api/?results=12&nat=us';
 const gallery = document.getElementById('gallery');
+const body = document.querySelector('body');
+
 
 async function getJSON(url) {
   try {
@@ -12,37 +14,69 @@ async function getJSON(url) {
 
 async function getPeople(url) {
   const peopleJSON = await getJSON(url);
-  const results = await peopleJSON.results;
-  generateHTML(results);
+  return results = await peopleJSON.results;
 }
 
 function generateHTML(data) {
   data.map(person => {
-    const section = document.createElement('section');
-    gallery.appendChild(section);
-    section.innerHTML = `
-      <div class="card">
-        <div class="card-img-container">
-          <img class="card-img" src="${person.picture.large}" alt="profile picture">
+    gallery.insertAdjacentHTML('beforeend', `
+      <div class="card click">
+        <div class="card-img-container click">
+          <img class="card-img click" src="${person.picture.large}" alt="profile picture">
         </div>
-        <div class="card-info-container">
-          <h3 id="name" class="card-name cap">${person.name.first} ${person.name.last}</h3>
-          <p class="card-text">${person.email}</p>
-          <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+        <div class="card-info-container click">
+          <h3 id="name" class="card-name cap click">${person.name.first} ${person.name.last}</h3>
+          <p class="card-text click">${person.email}</p>
+          <p class="card-text cap click">${person.location.city}, ${person.location.state}</p>
         </div>
       </div>
-    `;
+    `)
   });
+  return data;
+}
+
+function formatTelephone(text) {
+  const regex = /^\D*(\d{3})\D*(\d{3})\D*(\d{4})\D*$/;
+  return text.replace(regex, '($1) $2-$3');
+}
+
+function formatDOB(text) {
+  const regex = /^(\d{4})\D*(\d{2})\D*(\d{2}).*$/;
+  return text.replace(regex, '$2/$3/$1');
 }
 
 function generateModal(data) {
-
+  const modalContainer = document.createElement('div');
+  modalContainer.classList.add('modal-container');
+  body.appendChild(modalContainer);
+  modalContainer.insertAdjacentHTML('beforeend', `
+    <div class="modal">
+      <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
+      <div class="modal-info-container">
+        <img class="modal-img" src="${data.picture.large}" alt="profile picture">
+        <h3 id="name" class="modal-name cap">${data.name.first} ${data.name.last}</h3>
+        <p class="modal-text">${data.email}</p>
+        <p class="modal-text cap">${data.location.city}</p>
+        <hr>
+        <p class="modal-text">${formatTelephone(data.phone)}</p>
+        <p class="modal-text">${data.location.street.number} ${data.location.street.name}, ${data.location.state}, ${data.location.postcode}</p>
+        <p class="modal-text">Birthday: ${formatDOB(data.dob.date)}</p>
+      </div>
+    </div>
+  `);
+  
+  const modal = document.querySelector('.modal-container');
+  const btn = document.querySelector('button');
+  btn.addEventListener('click', (e) => {
+    modal.remove();
+  })
 }
 
-getPeople(url);
-
-gallery.addEventListener('click', (event) => {
-  if (event.target.className === 'card') {
-    console.log('hit');
-  }
-});
+getPeople(url)
+  .then(generateHTML)
+  .then((data) => {
+    const card = document.querySelectorAll('.card');
+    for(let i = 0; i < data.length; i++) {
+      card[i].addEventListener('click', () => generateModal(data[i]));
+    }
+  });
